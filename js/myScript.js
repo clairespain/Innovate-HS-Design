a37a1253f26310b00aa9eaaa01fdfb57e38face8
Clarissa Spain <35749695+ClarissaSpain@users.noreply.github.com>
2020-02-23 23:01:30 -0700

    pagination wip

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,6 @@
 // Feedback
 
-function fn1(){
+function fn1() {
   var myQuestion = document.getElementById("question");
   myQuestion.innerHTML = "What could have been better?";
   document.getElementById("mainImage").src = "../img/comments.svg";
@@ -8,31 +8,33 @@ function fn1(){
   document.getElementById("btnsubmit2").style.display = "block";
 }
 
-function fn2(){
+function fn2() {
   var myQuestion = document.getElementById("question");
   myQuestion.innerHTML = "Would you like to hear more about UM offerings for high school students?";
   document.getElementById("mainImage").src = "../img/comments.svg";
-  document.getElementById("comment").style.display="none";
+  document.getElementById("comment").style.display = "none";
   document.getElementById("btnsubmit2").style.display = "none";
   document.getElementById("btnsubmit3").style.display = "block";
   document.getElementById("btnsubmit4").style.display = "block";
 }
 
-function fn3(){
+function fn3() {
   var myQuestion = document.getElementById("question");
   myQuestion.innerHTML = "Other comments, questions, or concerns...";
   document.getElementById("mainImage").src = "../img/comments.svg";
-    document.getElementById("comment").style.display="block";
-    document.getElementById("btnsubmit3").style.display = "none";
-    document.getElementById("btnsubmit4").style.display = "none";
+  document.getElementById("comment").style.display = "block";
+  document.getElementById("btnsubmit3").style.display = "none";
+  document.getElementById("btnsubmit4").style.display = "none";
   document.getElementById("btnsubmit5").style.display = "block";
 }
 
-function fn4(){
+function fn4() {
   var myQuestion = document.getElementById("question");
   myQuestion.innerHTML = "We thank you for your time spent taking this survey. Your response has been recorded. Click 'Next' to continue.";
   document.getElementById("mainImage").src = "../img/completed.svg";
-  document.getElementById("comment").style.display="none";
+  document.getElementById("comment").style.display = "none";
   // document.getElementById("btnsubmit2").style.display = "none";
   // document.getElementById("btnsubmit3").style.display = "block";
 }
+
+//Swipe/pagination
